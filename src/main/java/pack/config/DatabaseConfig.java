3226fc4263b1b66ecf351a8c6b8d4787c003970a
Nikita <nikiforovpizza@gmail.com>
2018-06-25 00:26:57 +0300

    Heroku configuration

---
+++ src/main/java/pack/config/DatabaseConfig.java
@@ -0,0 +1,22 @@
+package pack.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DatabaseConfig {
+//    @Bean
+//    public DataSource dataSource() throws URISyntaxException {
+//        URI dbUri = new URI(System.getenv("mysql://bf6656e338de91:ddf40741@eu-cdbr-west-02.cleardb.net/heroku_e77eb3c124ce0a9?reconnect=true"));
+//
+//        String username = dbUri.getUserInfo().split(":")[0];
+//        String password = dbUri.getUserInfo().split(":")[1];
+//        String dbUrl = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath();
+//
+//        DriverManagerDataSource basicDataSource = new DriverManagerDataSource();
+//        basicDataSource.setUrl(dbUrl);
+//        basicDataSource.setUsername(username);
+//        basicDataSource.setPassword(password);
+//
+//        return basicDataSource;
+//    }
+}
